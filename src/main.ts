4a091242cbbcb5dedeab475e62f3f9625a962ca1
haris <talhahkhaan@gmail.con>
2026-01-26 21:56:41 +0500

    project_basic_setup_feature_demo

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { Post } from './components/post/post';
+
+const app = document.getElementById('app')!;
+
+const post1 = new Post("Hello World", "This is my first post!");
+post1.attach(app);
+
+const post2 = new Post("Second Post", "Learning TypeScript + Bootstrap is fun!");
+post2.attach(app);
